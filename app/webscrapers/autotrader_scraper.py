a7283a46928a19add60acf4c096020fecdcfac5c
amadeusz-zackiewicz <amadeusz.zackiewicz@gmail.com>
2022-07-04 11:44:28 +0100

    Add convenience variable inside search method

--- app/webscrapers/autotrader_scraper.py
+++ app/webscrapers/autotrader_scraper.py
@@ -45,38 +45,41 @@ class AutotraderWebscraper(WebScraperBase):
         super().select_drop_down_by_value(element, target)
 
     def search(self):
-        ### Post code
-        element = self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="postcode"]')
-        element.click()
-        element.send_keys(self.config["search"]["Postcode"])
-        ### Distance
-        self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="distance"]'), self.config["search"]["Distance"])
 
-        make = self.config["search"]["Make"]
+        config = self.config["search"]
+
+        ### Post code
+        self.input_text(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="postcode"]'), config["Postcode"])
+        ### Distance
+        self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="distance"]'), config["Distance"])
+
+        make = config["Make"]
         
         ### Make
         if make != "Any":
-            model = self.config["search"]["Model"]
+            model = config["Model"]
             self.select_drop_down_by_value(self.GET_TYPE_XPATH, '//*[@id="make"]', make)
             self.select_drop_down_by_value(self.GET_TYPE_XPATH, '//*[@id="model"]', model)
             ### Model
             if model != "Any":
                 ### Model variant
-                self.select_drop_down_by_value(self.GET_TYPE_XPATH, '//*[@id="aggregatedTrim"]', self.config["search"]["Model variant"])
+                self.select_drop_down_by_value(self.GET_TYPE_XPATH, '//*[@id="aggregatedTrim"]', config["Model variant"])
 
         ### Buying with
         element = self.driver.find_element(self.GET_TYPE_XPATH, '//*[@data-gui="search-price-panel"]')
-        buying_with = self.config["search"]["Buying with"]
+        buying_with = config["Buying with"]
         self.auto_trader_click_wide_toggle(element, buying_with.lower())
         
-        if buying_with == "Finance":
-            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="minMonthlyPrice"]'), self.config["search"]["Min price"])
-            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="maxMonthlyPrice"]'), self.config["search"]["Max price"])
-            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="finance-deposit"]'), self.config["search"]["Finance"]["Deposit"])
-            self.auto_trader_click_wide_toggle(element, self.config["search"]["Finance"]["Term"])
-            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="finance-mileage"]'), self.config["search"]["Finance"]["Mileage"])
-        else:
-            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="minPrice"]'), self.config["search"]["Min price"])
-            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="maxPrice"]'), self.config["search"]["Max price"])
+        if buying_with == "Finance": ## Finance
+            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="minMonthlyPrice"]'), config["Min price"])
+            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="maxMonthlyPrice"]'), config["Max price"])
+            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="finance-deposit"]'), config["Finance"]["Deposit"])
+            self.auto_trader_click_wide_toggle(element, config["Finance"]["Term"])
+            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="finance-mileage"]'), config["Finance"]["Mileage"])
+        else: ## Cash
+            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="minPrice"]'), config["Min price"])
+            self.select_drop_down_by_value(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="maxPrice"]'), config["Max price"])
 
-        element = self.hover_and_click_element(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@data-gui="search-cars-button"]'))
+        #self.input_text(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@id="keywords"]'), config["Keywords"])
+
+        #element = self.hover_and_click_element(self.driver.find_element(self.GET_TYPE_XPATH, '//*[@data-gui="search-cars-button"]')) # Press search button
