7ab27b49f5091767d2088a2c92611ca49fd0ee25
amadeusz-zackiewicz <amadeusz.zackiewicz@gmail.com>
2022-07-16 04:47:45 +0100

    Remove old code from main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from app.webscrapers.autotrader_scraper import AutotraderWebscraper
-import os
-import platform
 
 if __name__ == "__main__":
     scraper = AutotraderWebscraper("autotrader_london_red_hatchbacks", headless=False)
@@ -11,10 +9,3 @@ if __name__ == "__main__":
     scraper.scrape_links()
     scraper.scrape_all_details()
     scraper.close()
-
-
-    project_root_path = os.path.dirname(os.path.abspath(__file__))
-    if platform.system == "Linux":
-        os.popen(f"xdg-open {project_root_path}/data/results.csv")
-    elif platform.system == "Windows":
-        os.popen(f"{project_root_path}/data/results.csv")
